705c01faccc2fb76c60e5f0f7fd06bef3428c23d
Manuel Hernández <carlosmanuel.hernandez@bosonit.com>
2022-04-27 12:25:01 +0200

    Feat: Detalle delete(necesita comprobar error)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTableModule} from '@angular/material/table';
 import {MatIconModule} from '@angular/material/icon';
@@ -12,18 +14,22 @@ import {MatIconModule} from '@angular/material/icon';
 import { HomeComponent } from './home/home.component';
 import { Error404Component } from './shared/error404/error404.component'
 import { ListadoPersonasComponent } from './personas/componentes/listado-personas/listado-personas.component';
+import { FormularioPersonasComponent } from './personas/componentes/formulario-personas/formulario-personas.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     Error404Component,
-    ListadoPersonasComponent
+    ListadoPersonasComponent,
+    FormularioPersonasComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     HttpClientModule,
     MatIconModule,
     MatTableModule,
